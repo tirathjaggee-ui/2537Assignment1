d81fe106d0afa2c40a0c110a83e4be69d5666da9
Tirath <tjaggee@my.bcit.ca>
2026-05-05 21:54:03 -0700

    had dupe ports

--- app.js
+++ app.js
@@ -239,7 +239,6 @@ app.use((req, res) => {
 });
 
 
-const PORT = process.env.PORT || 10000;
 // start server
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
